55ffce750f6321c153665a78fc3d24d97099e06d
Alex <alexsb@mail.ru>
2022-07-20 15:32:37 +0300

    wifi only

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <Ticker.h>
 #include "HCSR04.h"
 #include "filter.h"
@@ -13,8 +12,6 @@ const char* ssid = "theflat";
 const char* password = "sheludko";
 
 
-ESP8266WebServer server(80);
-String webPage = "";
 
 Ticker tick;
 float distance=0;
@@ -52,48 +49,12 @@ void setup() {
     Serial.println("WiFi Connected");
     Serial.printf("Local IP:   "); Serial.println(WiFi.localIP() );
     
-    /*
-    if (mdns.begin("esp8266", WiFi.localIP())) {
-      Serial.println("MDNS responder started");
-               //  "Запущен MDNSresponder"
-      }
-    */  
-  
-    webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
-    webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
-  
-    server.on("/", [](){
-        server.send(200, "text/html", webPage);
-    });
-    server.on("/socket1On", [](){
-        server.send(200, "text/html", webPage);
-        digitalWrite(LED_BUILTIN, LOW );
-        delay(1000);
-    });
-    server.on("/socket1Off", [](){
-        server.send(200, "text/html", webPage);
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000); 
-    });
-    server.on("/socket2On", [](){
-        server.send(200, "text/html", webPage);
-        digitalWrite(LED_BUILTIN, LOW );
-        delay(1000);
-    });
-    server.on("/socket2Off", [](){
-        server.send(200, "text/html", webPage);
-        digitalWrite(LED_BUILTIN, HIGH );
-        delay(1000); 
-    });
-
-    server.begin();
-    Serial.println("HTTP server started");
 
     tick.attach_ms(1000, tick_instance);
 
 }  // setup
  
 void loop(void){
-  server.handleClient();
+  
 }
 
